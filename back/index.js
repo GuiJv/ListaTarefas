7eef2a7c32198ec2c7382d7e3f42b0575bac0309
GuiJv <guilhermejosevinhas@gmail.com>
2023-10-16 22:26:02 -0300

    Add task endpoint

--- back/index.js
+++ back/index.js
@@ -1,13 +1,23 @@
 const fs = require('fs')
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
+const jsonParser = bodyParser.json();
 
 const port = 3001
 
 app.get('/', (req, res) =>{
     const filesJson = JSON.parse(fs.readFileSync('back/taskDB/tasks.json', 'utf-8'))
     res.send(filesJson)
+})
+
+app.post('/', jsonParser ,(req, res) =>{
+    const files = JSON.parse(fs.readFileSync('back/taskDB/tasks.json', 'utf-8'))
+    files.push(req.body)
+    fs.writeFileSync('back/taskDB/tasks.json', JSON.stringify(files))
+    res.status(200)
+    res.json("ok")
 })
 
 
